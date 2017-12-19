d992903a73fec21b8346f8f2329d5f7fd7f4936b
mdk250 <mike-dekok@hotmail.com>
2017-12-19 19:00:34 +0100

    Added iPad 5 wifi 10.3.1 offsets
    Right offsets for iPad 5 (2017) wifi iOS 10.3.1.

--- v0rtexNonce/offsets.m
+++ v0rtexNonce/offsets.m
@@ -650,8 +650,19 @@ void load_offsets(void)
     	//10.3.1
     	if(!strcmp(version, "14E304"))
     	{
-    		LOG("10.3.1 - 14E304 offsets not found for %s", device);
-    		exit(1);
+    	    OFFSET_ZONE_MAP                             = 0xfffffff007548478;
+            OFFSET_KERNEL_MAP                           = 0xfffffff0075a4050;
+            OFFSET_KERNEL_TASK                          = 0xfffffff0075a4048;
+            OFFSET_REALHOST                             = 0xfffffff00752aba0;
+            OFFSET_BZERO                                = 0xfffffff007081f80;
+            OFFSET_BCOPY                                = 0xfffffff007081dc0;
+            OFFSET_COPYIN                               = 0xfffffff007180720;
+            OFFSET_COPYOUT                              = 0xfffffff007180914;
+            OFFSET_IPC_PORT_ALLOC_SPECIAL               = 0xfffffff007099efc;
+            OFFSET_IPC_KOBJECT_SET                      = 0xfffffff0070ad154;
+            OFFSET_IPC_PORT_MAKE_SEND                   = 0xfffffff007099a20;
+            OFFSET_IOSURFACEROOTUSERCLIENT_VTAB         = 0xfffffff006e65CB8;
+            OFFSET_ROP_ADD_X0_X0_0x10                   = 0xfffffff006429174;
     	}
 
     	//10.3
